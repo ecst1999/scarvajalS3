c8c1c8bcf1ceab276a5a409615f4cba435962891
master <master@SMLAB108PC22_23>
2023-06-13 21:22:37 -0500

    Agregar archivos de proyecto.

---
+++ scarvajalS3/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace scarvajalS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string dato1 = txtDato1.Text;
+            double dato2 = Double.Parse(txtDato2.Text);
+
+            Navigation.PushAsync(new Page2(dato1, dato2));
+        }
+    }
+}
---
+++ scarvajalS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace scarvajalS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1, double dato2)
+        {
+            InitializeComponent();
+            lblDato1.Text = "DATO 1 =======>" + dato1;
+            txtDato2.Text = "DATO 2 =======>" + dato2.ToString();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
